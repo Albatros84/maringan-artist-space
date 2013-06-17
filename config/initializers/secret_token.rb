5d1e2d5afc3dd7f32cb58db2b1e386aed089eb4f
Janusz Liro <janlir@wp.pl>
2013-06-17 14:15:59 +0200

    generated app with rails 4rc2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MaringanArtistSpace::Application.config.secret_key_base = '7b658c86508eb17cfbee6e51c19ba52c4d7720bcceadd002cdecb78dbabf082931c0e9f8391013622a4466f54da917c76f31a2bf7e3efe09928ca547caa97a98'
